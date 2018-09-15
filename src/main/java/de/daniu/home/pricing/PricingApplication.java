87cfec69f503cccdcf1a97905439e76b4c6a4bd3
joern <joern.schumacher@acando.de>
2018-09-15 12:29:06 +0200

    Using spring config server, adjusting table name

--- src/main/java/de/daniu/home/pricing/PricingApplication.java
+++ src/main/java/de/daniu/home/pricing/PricingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class PricingApplication {
+public class PricingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PricingApplication.class, args);
--- src/main/java/de/daniu/home/pricing/database/TarifEntity.java
+++ src/main/java/de/daniu/home/pricing/database/TarifEntity.java
@@ -5,11 +5,13 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 @Entity
+@Table(name="tarife")
 public class TarifEntity implements Tarif {
     @Id
     @NotNull
